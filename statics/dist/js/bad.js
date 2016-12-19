010ba2d98979b69468d774da434900f80505ebdc
subying <tearlight2008@gmail.com>
2016-12-19 09:57:10 +0800

    修改eslint 规则

--- statics/dist/js/bad.js
+++ statics/dist/js/bad.js
@@ -2,9 +2,10 @@
  *@fileOverflow 测试一下
  */
 class Test{
-    /** @description gonneng
+    /**
+     * @description gonneng
      * It returns test + 10
-     * @returns {int} sum of test and 10
+     * @returns {int}
      */
     getDate(){}
 }
